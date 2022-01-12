5314d6abe6e0de30104e658ecdda1c3303d251d7
Kent Chen <kchen21@nait.ca>
2022-01-12 16:22:36 -0700

    salesAPPchange

--- SalesApp/SalesApp/Program.cs
+++ SalesApp/SalesApp/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*  Purpose: Compute the total of a sales item using the price and tax.
+ *  
+ *  Inputs:  price in dollars and cents
+ *  
+ *  Outputs: sales tax
+ *           sales total
+ *  
+ *  Algorithm: Step 1) Prompt and read in the sale price
+ *             Step 2) Calculate the sale total using the formula:
+ *                            salesTax = salePrice * 0.05
+ *                            salesTotal = salePrice + salesTax
+ *             Step 3) Display the computed sale total and total
+ *             
+ *  Last modified:  2022-01-12
+ *  */
+using System;
 
 namespace SalesApp
 {
@@ -10,16 +25,26 @@ namespace SalesApp
             double price,
                     tax,
                     total;
+            const double salesTax = 0.05;
+            string userinput;
 
-            // Assign values to the variables for price, tax, and total
-            price = 29.75;
-            tax = 1.76;
-            total = 31.51;
+            // Prompt and read in the sale price
+            Console.Write("Enter the price of the item:");
+            userinput = Console.ReadLine();
 
-            // Write to the screen the price, tax, and total.
+            // Calculate the sale total using the formula
+            price = double.Parse(userinput);
+            tax = price * salesTax;
+            total = price + tax;
+
+            // Display the computed sale total and total
             Console.WriteLine($"The price of the item is {price:C}");
             Console.WriteLine($"The tax is {tax:C}");
             Console.WriteLine($"The total is {total:C}");
+
+            Console.WriteLine($"{"Price:", -10}{price,8:c}");
+            Console.WriteLine($"{"Tax:",-10}{tax,8:c}");
+            Console.WriteLine($"{"Total:",-10}{total,8:c}");
         }
     }
 }
