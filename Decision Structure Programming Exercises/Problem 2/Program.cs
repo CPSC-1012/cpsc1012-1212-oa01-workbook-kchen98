e67e65847b3f843b23ae6c75201c42c98921f811
Kent Chen <kchen21@nait.ca>
2022-01-26 14:57:17 -0700

    Problem 2 and 3 added

---
+++ Decision Structure Programming Exercises/Problem 2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*  Purpose=    Determine if a year entered by the user is a leap year
+ *  
+ *  Input=  Year
+ *  
+ *  Output= True or False
+ *  
+ *  Algorithm:  Step 1: Prompt the user to enter a year.
+ *              Step 2: Use if statement to determine if a year is a leap year.
+ * */
+using System;
+
+namespace Problem_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter a year.
+            Console.WriteLine("Enter a year to determine if it's a leap year: ");
+            int userYear = int.Parse(Console.ReadLine());
+
+            // Use if statement to determine if a year is a leap year.
+            if (userYear % 4 == 0 && userYear % 100 != 0 || userYear % 400 == 0)
+            {
+                Console.WriteLine($"True, {userYear} is a leap year");
+            }
+            else
+            {
+                Console.WriteLine($"False, {userYear} is not leap year");
+            }
+        }
+    }
+}
---
+++ Decision Structure Programming Exercises/Problem 3/Program.cs
@@ -0,0 +1,126 @@
+﻿/*  Purpose:    Create a rock paper scissors game with the computer as the opponent
+ *  
+ *  Input:  Scissor (0), Rock (1), Paper(2)
+ *  
+ *  Output: Win, Lose or Draw
+ *  
+ *  Algorithm:  Step 1: Randomly generate an integer from 0 to 2 that will represent rock, paper and
+ *                          scissors.
+ *              Step 2: Prompt the user to enter their choice.
+ *              Step 3: Use switch statement to assign the integers one of the signs.
+ *              Step 4: Use switch and if statements to test whether the user choice will result in a
+ *                          win, lose or draw.
+ *              Step 5: Displays the results
+ **/
+using System;
+
+namespace Problem_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Randomly generate an integer from 0 to 2 that will represent rock, paper and
+                         scissors. */
+            Random game = new Random();
+            int computerChoice = game.Next(0,3);
+
+            // Prompt the user to enter their choice.
+            Console.WriteLine(computerChoice);
+            Console.WriteLine("You will be playing rock paper scissors with the computer");
+            Console.WriteLine("Scissor (0), Rock (1), Paper(2)");
+            Console.Write("Enter the number corresponding to your pick: ");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            // Use switch statement to assign the integers one of the signs.
+            string computerSign,
+                   userSign,
+                   results = "";
+            switch (userChoice)
+            {
+                case 0:
+                    userSign = "scissors";
+                    break;
+                case 1:
+                    userSign = "rock";
+                    break;
+                case 2:
+                    userSign = "paper";
+                    break;
+                default:
+                    userSign = "?";
+                    break;
+            }
+
+            /* Use switch and if statements to test whether the user choice will result in a
+                          win, lose or draw. */
+            switch (computerChoice)
+            {
+                case 0:
+                    computerSign = "scissors";
+                    if (userChoice == 0)
+                    {
+                        results = "It is a draw.";
+                    }
+                    else if (userChoice == 1)
+                    {
+                        results = "You win.";
+                    }
+                    else if (userChoice == 2)
+                    {
+                        results = "You lose.";
+                    }
+                    else
+                    {
+                        results = "Invalid Results";
+                    }
+                    break;
+                case 1:
+                    computerSign = "rock";
+                    if (userChoice == 0)
+                    {
+                        results = "You lose.";
+                    }
+                    else if (userChoice == 1)
+                    {
+                        results = "It is a draw.";
+                    }
+                    else if (userChoice == 2)
+                    {
+                        results = "You win.";
+                    }
+                    else
+                    {
+                        results = "Invalid Results";
+                    }
+                    break;
+                case 2:
+                    computerSign = "paper";
+                    if (userChoice == 0)
+                    {
+                        results = "You win.";
+                    }
+                    else if (userChoice == 1)
+                    {
+                        results = "You lose.";
+                    }
+                    else if (userChoice == 2)
+                    {
+                        results = "It is a draw.";
+                    }
+                    else
+                    {
+                        results = "Invalid Results";
+                    }
+                    break;
+                default:
+                    computerSign = "";
+                    break;
+
+            }
+
+            // Displays the results
+            Console.WriteLine($"The computer is {computerSign}. You are {userSign}. {results}");
+        }   
+    }
+}
