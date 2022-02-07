1b3058e78be61032b27bf77717e2e9a560d8a67c
Kent Chen <kchen21@nait.ca>
2022-02-07 16:45:55 -0700

    feb 7

---
+++ Temperature Table/Temperature Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Temperature_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minValue,
+                maxValue;
+
+            Console.Write("Enter starting celsius value: ");
+            minValue = int.Parse(Console.ReadLine());   
+
+            Console.Write("Enter ending celsius value: ");
+            maxValue = int.Parse(Console.ReadLine());
+
+            for (int celsius = minValue; celsius <= maxValue; celsius++)
+            {
+                double fahrenheit = (celsius * 9.0 / 5) + 32;
+                Console.WriteLine($"{celsius,-10}{fahrenheit,10:f2}");
+            }
+        }
+    }
+}
