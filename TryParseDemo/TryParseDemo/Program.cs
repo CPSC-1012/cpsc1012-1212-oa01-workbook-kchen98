a64ef0b2e865601a95154aadc2a5045264e13e56
Kent Chen <kchen21@nait.ca>
2022-02-09 16:33:44 -0700

    tryparse demo

---
+++ TryParseDemo/TryParseDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TryParseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hockeyPlayerNumber;
+            bool validInput = false;
+
+            Console.Write("Enter the hockey player number (1-99): ");
+            validInput = int.TryParse(Console.ReadLine(), out hockeyPlayerNumber);
+
+            while (!validInput || hockeyPlayerNumber < 1 || hockeyPlayerNumber > 99)
+            {
+                Console.WriteLine("Invalid input value. Try again.");
+                Console.Write("Enter the hockey player number (1-99): ");
+                validInput = int.TryParse(Console.ReadLine(), out hockeyPlayerNumber);
+            }
+            Console.WriteLine($"The hockey player number is {hockeyPlayerNumber}");
+        }
+    }
+}
