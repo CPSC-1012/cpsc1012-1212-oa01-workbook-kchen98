c22cc5a81fd85964f652a01784b26c5936b379cb
Kent Chen <kchen21@nait.ca>
2022-02-28 17:31:00 -0700

    average(method)

---
+++ Average(Method)/Average(Method)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Average_Method_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int count = 0;
+            const int SentinelValue = 999;
+            bool sentinelValueEntered = false;
+            double average = 0;
+
+            while (!sentinelValueEntered)
+            {
+                int number = PromptForPositiveNumber("Enter a number: ");
+                if (number != SentinelValue)
+                {
+                    count++;
+                    sum += number;
+                }
+                else
+                {
+                    sentinelValueEntered = true;
+                }
+            }
+            if (count > 0)
+            {
+                average = sum / count;
+                Console.WriteLine($"Average of input is {average:f1}");
+            }
+        }
+
+        static int PromptForPositiveNumber(string message)
+        {
+            int userNumber = 0;
+            bool validUserNumber = false;
+
+            do
+            {
+                Console.WriteLine(message);
+                validUserNumber = int.TryParse(Console.ReadLine(), out userNumber);
+                if (!validUserNumber || userNumber < 0)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }while (!validUserNumber || userNumber < 0);
+            return userNumber;
+        }
+
+
+    }
+}
