22e0a72597520883df4311f2a57a20350b525769
Kent Chen <kchen21@nait.ca>
2022-03-21 16:04:10 -0600

    stream reader

--- FileIODemo/FileIODemo/Program.cs
+++ FileIODemo/FileIODemo/Program.cs
@@ -4,6 +4,76 @@ namespace FileIODemo
 {
     internal class Program
     {
+        static string DataFilePath = "../../../points.txt";
+
+        static int ReadFromFileUsingStreamReader(string[] nameArray, int[] pointArray)
+        {
+            int recordCount = 0;
+            // Check if the data file exists
+            if (!File.Exists(DataFilePath))
+            {
+                Console.WriteLine($"The file ${DataFilePath} does not exists.");
+            }
+            else
+            {
+                try
+                {
+                    StreamReader reader = new StreamReader(DataFilePath);
+                    int index = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        string currentLine = reader.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(currentLine))
+                        {
+                            // Return an array where each value is separated by a comma
+                            string[] tokens = currentLine.Split(',');
+                            nameArray[index] = tokens[0];
+                            pointArray[index] = int.Parse(tokens[1]);
+                            index++;
+                            recordCount++;
+                        }
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading from file with exception {ex.Message}");
+                }
+            }
+                return recordCount;
+        }
+        static int ReadFromFileUsingReadAllLines(string[] nameArray, int[] pointArray)
+        {
+            int recordCount = 0; // number of records read from file
+
+            // Check if the data file exists
+            if (!File.Exists(DataFilePath))
+            {
+                Console.WriteLine($"The file ${DataFilePath} does not exists.");
+            }
+            else
+            {
+                try
+                {
+                    // Read all the lines from the text file and return an array of lines
+                    string[] allLines = File.ReadAllLines(DataFilePath);
+                    const char Delimiter = ',';
+                    for (int index = 0; index < allLines.Length && index < nameArray.Length; index++)
+                    {
+                        string currentLine = allLines[index];
+                        string[] tokens = currentLine.Split(Delimiter);
+                        nameArray[index] = tokens[0];
+                        pointArray[index] = int.Parse(tokens[1]);
+                        recordCount++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Write($"Error reading from file with exception {ex.Message}");
+                }
+            }
+            return recordCount;
+        }
         static void Main(string[] args)
         {
             // Declare and create two parallel arrays for student names and student points
@@ -12,17 +82,8 @@ namespace FileIODemo
             string[] nameArray = new string[MaxStudents];
             int[] pointArray = new int[MaxStudents];
 
-            // Read all the lines from the text file and return an array of lines
-            string[] allLines = File.ReadAllLines(@"D:\cpsc_temp\points.txt");
-            const char Delimiter = ',';
-            for (int index = 0; index < allLines.Length && index < MaxStudents; index++)
-            {
-                string currentLine = allLines[index];
-                string[] tokens = currentLine.Split(Delimiter);
-                nameArray[index] = tokens[0];   
-                pointArray[index] = int.Parse(tokens[1]);  
-                currentStudentCount++;
-            }
+            //currentStudentCount = ReadFromFileUsingReadAllLines(nameArray, pointArray);
+            currentStudentCount = ReadFromFileUsingStreamReader(nameArray, pointArray);
             DisplayStudents(nameArray, pointArray, currentStudentCount);
         }
 
