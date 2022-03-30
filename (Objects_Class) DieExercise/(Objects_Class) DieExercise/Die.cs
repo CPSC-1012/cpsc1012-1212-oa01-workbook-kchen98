7fe6ace20e07bbe45bfbbee5d1b048df3fe24541
Kent Chen <kchen21@nait.ca>
2022-03-30 16:07:07 -0600

    Die OOP

---
+++ (Objects_Class) DieExercise/(Objects_Class) DieExercise/Die.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Objects_Class__DieExercise
+{
+    public class Die
+    {
+        // Declare data fields for storing data
+        private int _sides; // number of sides for the Die
+        private int _value; // face value of the Die
+
+        // Properties to encapsulate access to the data fields
+        public int Sides
+        {
+            get // A read-only property contains only an acccessor
+            {
+                return _sides;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+
+        // Define constructors to assign meaningful values to the fields/properties
+        //public Die() // Default constructor but at this exercise it's not required
+        //{
+            //_sides = 6;
+        //}
+        public Die(int newSides)
+        {
+            _sides = newSides;
+            Roll();
+        }
+
+        public void Roll()
+        {
+            Random random = new Random();
+            _value = random.Next(1, Sides + 1);
+        }
+    }
+}
---
+++ (Objects_Class) DieExercise/(Objects_Class) DieExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _Objects_Class__DieExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a 6 sided Die and a 12 sided Die
+            Die sixSidedDie = new Die(6);
+            Die twelveSidedDie = new Die(12);
+
+            Console.WriteLine($"{sixSidedDie.Value, 10} {twelveSidedDie.Value, 10}");
+            // Simulate rolling the two Die 5 times
+            Console.WriteLine($"Rolling the two Die 5 times. ");
+            for (int index = 0; index < 5; index++)
+            {
+                sixSidedDie.Roll();
+                twelveSidedDie.Roll();
+                Console.WriteLine($"{sixSidedDie.Value,10} {twelveSidedDie.Value,10}");
+            }
+        }
+    }
+}
